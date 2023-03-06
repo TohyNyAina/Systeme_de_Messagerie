d28c7c969f9d108beae022efa99150cf547a1446
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-03-06 09:50:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ConversationsController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes call
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
